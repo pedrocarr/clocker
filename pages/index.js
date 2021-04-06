ee1d0bd457bff77214269fddfba427cdad7e5d29
pedrocarr <eng.pedro.amb@gmail.com>
2021-04-06 14:06:12 -0300

    form control and features

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
-import { Container, Box, Button, Input, Text  } from '@chakra-ui/react'
-import { Logo } from './../components'
-
+import {
+  Container,
+  Box,
+  Button,
+  Input,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { Logo } from "./../components";
 
 export default function Home() {
   return (
     <Container p={4} centerContent>
       <Logo />
       <Box>
-        <Text>Crie sua agenda compartilhada</Text>
+        <Text p={4} mt={8}>
+          Crie sua agenda compartilhada
+        </Text>
       </Box>
+
+      <FormControl id="email" isRequired>
+        <FormLabel> Email address </FormLabel> <Input type="email" />
+        <FormHelperText> We 'll never share your email.</FormHelperText>
+      </FormControl>
     </Container>
-  )
+  );
 }
