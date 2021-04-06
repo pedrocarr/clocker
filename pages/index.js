143a5af576e3576cb5aa1d8b9a7192ff388af8f2
pedrocarr <eng.pedro.amb@gmail.com>
2021-04-06 13:58:16 -0300

    Text and Box

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import { Container, Box, Button, Input  } from '@chakra-ui/react'
+import { Container, Box, Button, Input, Text  } from '@chakra-ui/react'
 import { Logo } from './../components'
 
 
 export default function Home() {
   return (
-    <Container>
+    <Container p={4} centerContent>
       <Logo />
+      <Box>
+        <Text>Crie sua agenda compartilhada</Text>
+      </Box>
     </Container>
   )
 }
